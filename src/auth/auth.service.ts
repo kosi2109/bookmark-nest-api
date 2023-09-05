fe1c900ebdc5e537ad1695cd340efff215ce8bf7
viabells-sithuhtet <sithuhtet@i.viabells.com>
2023-09-05 23:19:12 +0630

    done auth

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,27 +1,39 @@
 import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
-import * as argan from "argon2";
+import * as argan from 'argon2';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService
+  ) {}
 
   async login(dto: AuthDto) {
-    const user = await this.prisma.user.findFirst({where : {
-      email : dto.email
-    }});
+    const user = await this.prisma.user.findFirst({
+      where: {
+        email: dto.email,
+      },
+    });
 
-    if (!user) throw new ForbiddenException("Credential Incorrect");
-    
+    if (!user) throw new ForbiddenException('Credential Incorrect');
+
     const pwMatch = await argan.verify(user.hash, dto.password);
-    
-    if (!pwMatch) throw new ForbiddenException("Credential Incorrect");
+
+    if (!pwMatch) throw new ForbiddenException('Credential Incorrect');
 
     delete user.hash;
 
-    return user;
+    const jwt = await this.signToken(user.id, user.email);
+
+    return {
+      access_token : jwt
+    };
   }
 
   async register(dto: AuthDto) {
@@ -30,21 +42,31 @@ export class AuthService {
 
     try {
       const user = await this.prisma.user.create({
-        data : {
-          email : dto.email,
-          hash : hash
-        }
-      })
-  
+        data: {
+          email: dto.email,
+          hash: hash,
+        },
+      });
+
       delete user.hash;
-  
+
       return user;
     } catch (error) {
       if (error instanceof PrismaClientKnownRequestError) {
-        if (error.code === "P2002") {
+        if (error.code === 'P2002') {
           throw new ForbiddenException('Credential taken');
         }
       }
     }
+  }
+
+  signToken(userId : number, email: string) : Promise<string> {
+    return this.jwt.signAsync({
+      sub : userId,
+      email
+    }, {
+      expiresIn : '15m',
+      secret : this.config.get('JWT_SECRET')
+    })
   }
 }
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { UserController } from './user.controller';
+import { JwtStrategy } from 'src/auth/strategy';
 
-@Module({})
+@Module({
+  controllers: [UserController],
+  providers: [JwtStrategy]
+})
 export class UserModule {}
